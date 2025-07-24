1d4055d7ed6f63348fd29b28d3f80b9994904d8e
Gaurav <gauravkumar732006@gmail.com>
2025-07-24 14:27:21 +0530

    Initial commit

--- BinarySearch/twosum.java
+++ BinarySearch/twosum.java
@@ -1,5 +1,29 @@
 package BinarySearch;
 
 public class twosum {
-    
+    public static int[] twosum(int[] nums, int target) {
+        // Check all pairs
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // If no solution found
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 6, 11, 7};
+        int target = 9;
+
+        int[] result = twosum(nums, target);
+
+        if (result[0] != -1) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
 }
