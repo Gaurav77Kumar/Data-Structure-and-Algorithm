2165a56722bdeac955aad8387ecdd77a9e962787
Gaurav <gauravkumar19114@gmail.com>
2025-08-09 04:05:46 +0530

    initial commit

---
+++ src/PATTERNS/P1.java
@@ -0,0 +1,29 @@
+package PATTERNS;
+
+
+
+//    *
+//    **
+//    ***
+//    ****
+//    *****
+
+
+public class P1 {
+    public static void main(String[] args) {
+        pattern1(6);
+
+    }
+
+    static void pattern1(int n){
+        for(int row = 1; row<=n; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
