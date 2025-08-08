dc94e1cf36173cf706bd2fa1dd006f9934de2d64
Gaurav <gauravkumar732006@gmail.com>
2025-08-09 03:54:11 +0530

    initial commit

---
+++ src/RECURSION/basic.java
@@ -0,0 +1,20 @@
+package RECURSION;
+
+public class basic {
+    public static void main(String[] args) {
+        print(1);
+    }
+    static void print(int n) {
+        if (n == 5) {
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+
+        // This is called tail recursion
+        // This is the last call
+        print(n + 1);
+    }
+}
+
+
