acd0c0a1f618680d29564423c4e0f1fb7e522936
Gaurav <gauravkumar19114@gmail.com>
2025-08-25 22:15:08 +0530

    Initial commit

---
+++ RECURSION/Skip_character.java
@@ -0,0 +1,25 @@
+package RECURSION;
+
+import java.sql.SQLOutput;
+
+public class Skip_character {
+    public static void main(String[] args) {
+        skip("", "baccab");
+
+    }
+
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+
+        }
+        char ch  = up.charAt(0);
+        if(ch  == 'a'){
+            skip(p, up.substring(1));
+        }
+        else{
+            skip(p + ch, up.substring(1));
+        }
+    }
+}
