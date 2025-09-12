64b91b7173f0cc719461c32b60d71f828a9668fe
Gaurav <gauravkumar19114@gmail.com>
2025-09-12 16:03:38 +0530

    Initial commit

---
+++ BACKTRACKING_MAZE_PROBLEMS/All_Paths.java
@@ -0,0 +1,43 @@
+package BACKTRACKING_MAZE_PROBLEMS;
+
+public class All_Paths {
+    public static void main(String[] args) {
+
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+
+        Allpath("", board, 0, 0);
+
+    }
+
+    static void Allpath(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+        // I am Considering this path in my Block
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            Allpath(p + 'D', maze, r + 1, c);
+        }
+        if (c < maze[0].length - 1) {
+            Allpath(p + 'R', maze, r, c + 1);
+        }
+        if( r > 0){
+            Allpath(p+ 'U', maze, r - 1, c);
+        }
+        if ( c > 0 ){
+            Allpath(p + 'L', maze, r, c - 1);
+        }
+        // this line is where the function will be over before the function get removed also remove the changes made ny function
+        maze[r][c] = true;
+    }
+}
