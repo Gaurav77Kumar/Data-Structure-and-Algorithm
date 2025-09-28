c3a48ba7dbff2dd9a132070971f642c8ff962308
Gaurav <gauravkumar19114@gmail.com>
2025-09-28 22:21:19 +0530

    Initial commit

---
+++ OOPS/a/Gretting.java
@@ -0,0 +1,7 @@
+package OOPS.a;
+
+public class Gretting {
+    public static void main(String[] args) {
+        System.out.println("Hii I am gaurav kumar");
+    }
+}
---
+++ OOPS/b/Greeting.java
@@ -0,0 +1,10 @@
+package OOPS.b;
+
+import static OOPS.a.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("How are you i am good");
+        message();
+    }
+}
