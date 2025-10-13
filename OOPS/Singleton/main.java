1eae4584ed279b5f838454ab84f66854ae19dca5
Gaurav <gauravkumar19114@gmail.com>
2025-10-14 02:54:36 +0530

    Initial commit

--- OOPS/Singleton/main.java
+++ OOPS/Singleton/main.java
@@ -2,7 +2,7 @@ package OOPS.Singleton;
 
 public class main {
     public static void main(String[] args) {
-        Singleton obj = Singleton.getInstance();
+        Singleton obj = Singleton.getInstance();  // Create an instance of Singleton class
 
 
     }
