9236510a66b30a560e9a3299c1cd53c1d92f4efd
Gaurav <gauravkumar19114@gmail.com>
2026-01-03 12:30:17 +0530

    initial commit

---
+++ src/LINKEDLIST/main.java
@@ -0,0 +1,49 @@
+package LINKEDLIST;
+
+public class main {
+    public static void main(String[] args) {
+//   LL list = new LL();
+
+//    list.insertFirst(3);
+//    list.insertFirst(2);
+//    list.insertFirst(1);
+
+//   list.insertLast(4);
+//   list.insertLast(5);
+//   list.insertLast(6);
+
+//   list.insertAtIndex(5,2);
+
+//        System.out.println("Deleted value: " + list.DeleteFirst());
+//        System.out.println("Deleted value at index 2: " + list.DeleteLast());
+//        System.out.println("Deleted value at index 1: " + list.DeleteAtIndex(1));
+
+//        list.display();
+
+
+
+//         DLL dll = new DLL();
+
+//         dll.insertFirst(3);
+//         dll.insertFirst(2);
+//         dll.insertFirst(1);
+
+//         dll.inserAtLast(4);
+//         dll.inserAtLast(5);
+
+//        dll.insert(7,2);
+//        dll.deleteFirst();
+
+
+//        CLL cll = new CLL();
+//        cll.insert(1);
+//        cll.insert(2);
+
+//        cll.delete(1);
+
+//         cll.display();
+
+
+
+    }
+}
