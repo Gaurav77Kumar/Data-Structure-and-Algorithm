d20538503e838c21c44013a8d7aa1a43bcae6fd1
Gaurav <gauravkumar19114@gmail.com>
2026-01-04 12:23:11 +0530

    Sort in linkedlist

---
+++ src/LINKEDLIST/MergeSort.java
@@ -0,0 +1,85 @@
+package LINKEDLIST;
+
+public class MergeSort {
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+    }
+
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+
+    // Find middle of linked list
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // break the list into two halves
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        return slow;
+    }
+
+
+    public static void main(String[] args) {
+        MergeSort ms = new MergeSort();
+
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+        head = ms.sortList(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
